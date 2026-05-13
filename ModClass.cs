6f44f7de1b052bf03c4b3a36afff209cfe40cd46
Joshua McAllister <joshnmcallister@gmail.com>
2026-05-13 11:03:45 -0700

    Initial commit

---
+++ ModClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Modding;
+using UnityEngine;
+using UObject = UnityEngine.Object;
+
+namespace EnemiesFightingEachOther
+{
+    public class EnemiesFightingEachOther : Mod
+    {
+        internal static EnemiesFightingEachOther Instance;
+
+        //public override List<ValueTuple<string, string>> GetPreloadNames()
+        //{
+        //    return new List<ValueTuple<string, string>>
+        //    {
+        //        new ValueTuple<string, string>("White_Palace_18", "White Palace Fly")
+        //    };
+        //}
+
+        //public EnemiesFightingEachOther() : base("EnemiesFightingEachOther")
+        //{
+        //    Instance = this;
+        //}
+
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+        {
+            Log("Initializing");
+
+            Instance = this;
+
+            Log("Initialized");
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# EnemiesFightingEachOther
+
+A mod for the game Hollow Knight.
