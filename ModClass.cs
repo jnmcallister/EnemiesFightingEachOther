dead89dbf32f9d628d450742ca3369b82fca3af0
Joshua McAllister <joshnmcallister@gmail.com>
2026-05-14 12:07:58 -0700

    Make spawning code more portable

--- ModClass.cs
+++ ModClass.cs
@@ -49,28 +49,7 @@ namespace EnemiesFightingEachOther
         {
             if (Input.GetKeyDown(KeyCode.O))
             {
-                Log("Spawning Gruz Mother");
-
-                // Instantiate enemy
-                GameObject newEnemy = UObject.Instantiate(preloads["GG_Gruz_Mother"]["_Enemies/Giant Fly"],
-                    HeroController.instance.transform.position, Quaternion.identity);
-                newEnemy.SetActive(true);
-
-                // Create a child object which will handle dealing damage to other enemies
-                // This is done because objects on the enemy layer don't collide with each other
-                GameObject damageEnemiesGO = new GameObject("Damage Enemies");
-                damageEnemiesGO.transform.SetParent(newEnemy.transform, false);
-                damageEnemiesGO.layer = 17; // Hero attack layer
-
-                // Let this enemy damage other enemies
-                DamageEnemies damageEnemies = damageEnemiesGO.AddComponent<DamageEnemies>();
-                damageEnemies.damageDealt = 10;
-                damageEnemies.ignoreInvuln = false;
-
-                // Give this child object a collider to interact with other enemies
-                Collider2D parentCol = newEnemy.GetComponent<Collider2D>();
-                Collider2D childCol = parentCol.CopyCollider2DTo(damageEnemiesGO);
-                childCol.isTrigger = true; // Make sure this doesn't collide with the ground
+                EnemySpawningUtils.SpawnEnemy(preloads["GG_Gruz_Mother"]["_Enemies/Giant Fly"]);
             }
         }
     }
